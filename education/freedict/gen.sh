f1d6412fe328a3506213a7f9242bc0395d04d913
mbuhl <git@moritzbuhl.de>
2023-10-12 20:56:35 +0200

    Annotate comment and license (CHECK AGAIN!) and write DESCR.

--- education/freedict/gen.sh
+++ education/freedict/gen.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
+ftp -o /tmp/db.xml https://freedict.org/freedict-database.xml
+
 # https://freedict.org/freedict-database.xml
 for d in $(grep -R dictd.tar.xz /tmp/db.xml | grep -o 'dictionaries\/[^/]*/[^/]*/' | cut -d/ -f 2-3); do
 	dict=$(echo ${d%%/*})
 	ver=$(echo ${d##*/})
+comment=$(/home/mbuhl/code/dictd/obj/dict -D $dict -d 00databaseshort | cut -d' ' -f2-)
+license=$(/home/mbuhl/code/dictd/obj/dict -D $dict -d 00database | grep -A2 -i license)
+
+clear
+echo "LICENSE: $license"
+read lic?'license: GPLv23+  Creative-Commons-foooooo:'
+
 cat << EOF > /usr/ports/mystuff/education/freedict/$dict/Makefile
-# XXX: comment lookup in dict
-# XXX: license?
-COMMENT =	free bilingual dictionary
+COMMENT =	$comment
 DICT =	$dict
 V =	$ver
 
-# GPLv2 or v3 or CC-XYZ or no real license or fuck
-PERMIT_PACKAGE =	Yes # XXX
-#PERMIT_DISTFILES =	Yes | Reason if No
+# $lic
+PERMIT_PACKAGE =	Yes
 
 .include "../Makefile.inc"
 .include <bsd.port.mk>
 EOF
 
-# XXX: write DESCR
+foo=$(/home/mbuhl/code/dictd/obj/dict -D $dict -d 00databaseinfo)
+echo "${foo##- }" > /usr/ports/mystuff/education/freedict/$dict/pkg/DESCR
 
 done
